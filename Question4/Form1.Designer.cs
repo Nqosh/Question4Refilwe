19d2f3b52f8c6f1d2c683f92345b3755210d809a
Nqobile <nqobilemoyo@capitecbank.co.za>
2021-05-14 20:45:49 +0200

    First Commit

---
+++ Question4/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Question4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMarkHeading = new System.Windows.Forms.Label();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnNextMark = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMarkHeading
+            // 
+            this.lblMarkHeading.AutoSize = true;
+            this.lblMarkHeading.Location = new System.Drawing.Point(12, 47);
+            this.lblMarkHeading.Name = "lblMarkHeading";
+            this.lblMarkHeading.Size = new System.Drawing.Size(155, 20);
+            this.lblMarkHeading.TabIndex = 0;
+            this.lblMarkHeading.Text = "Individual Assignment";
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(13, 89);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(154, 27);
+            this.txtMark.TabIndex = 2;
+            // 
+            // btnNextMark
+            // 
+            this.btnNextMark.Location = new System.Drawing.Point(13, 137);
+            this.btnNextMark.Name = "btnNextMark";
+            this.btnNextMark.Size = new System.Drawing.Size(154, 29);
+            this.btnNextMark.TabIndex = 3;
+            this.btnNextMark.Text = "Next Mark";
+            this.btnNextMark.UseVisualStyleBackColor = true;
+            this.btnNextMark.Click += new System.EventHandler(this.btnNextMark_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(13, 316);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(154, 35);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(196, 316);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(500, 35);
+            this.btnDisplay.TabIndex = 5;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Display Results :";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(191, 81);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(493, 216);
+            this.txtResults.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnNextMark);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.lblMarkHeading);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMarkHeading;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnNextMark;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ Question4/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Question4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] marks = new int[3];
+        int count = 0;
+
+        private int FindHighestMark(int inputOne, int inputTwo, int inputThree)
+        {
+            int highestMark = marks.Max();
+            return highestMark;
+        }
+
+        private int CalcAverage(int inputOne, int inputTwo, int inputThree)
+        {
+            int averageMark = (inputOne + inputTwo + inputThree) / marks.Length;
+            return averageMark;
+        }
+        private string Category(int input)
+        {
+            string message = string.Empty;
+
+            if (input < 40)
+            {
+                message = input + " "  +  "Failed";
+            }
+            if (input >= 40 && input < 50)
+            {
+                message = input + " " + "Supplementary.";
+            }
+            if (input >= 50 && input < 75)
+            {
+                message = input + " " + "Passed.";
+            }
+            if (input >= 75)
+            {
+                message = input + " " + "Passed with Distinction.";
+            }
+
+            return message;
+        }
+
+        private void Display(int averageMark, int highestMark)
+        {
+
+            string message = string.Empty;
+
+            message = Category(marks[0]);
+            string firstLine = "Individual Assignment Mark:" + ' ' + message + Environment.NewLine;
+
+            message = Category(marks[1]);
+            string secondLine = "Test Mark:" + ' ' + message + Environment.NewLine;
+
+            message = Category(marks[2]);
+            string thirdLine = "Group Assignment Mark:" +  " " + message + Environment.NewLine;
+
+            string fourthLine = "The highest mark is:" + " " + highestMark + Environment.NewLine;
+
+
+            string fifthLine = "Average mark is:" + " " + averageMark + Environment.NewLine;
+
+            txtResults.Text = firstLine + secondLine + thirdLine + fourthLine + fifthLine;
+        }
+
+        private void btnNextMark_Click(object sender, EventArgs e)
+        {
+            int inputMark = Convert.ToInt32(txtMark.Text);
+
+            if(inputMark > 100 || inputMark < 0 )
+            {
+                MessageBox.Show("Your Input Number Must between 0 and 100 ", "Error", MessageBoxButtons.OK);
+                txtMark.Text = "";
+                return;
+            }
+
+            switch (lblMarkHeading.Text)
+            {
+                case "Individual Assignment":
+                    marks[0] = inputMark;
+                    lblMarkHeading.Text = "Test Marks";
+                    txtMark.Text = "";
+                    break;
+                case "Test Marks":
+                    marks[1] = inputMark;
+                    lblMarkHeading.Text = "Group Assignment";
+                    txtMark.Text = "";
+                    break;
+                case "Group Assignment":
+                    marks[2] = inputMark;
+                    txtMark.Text = "";
+                    btnNextMark.Enabled = false;
+                    txtMark.Enabled = false;
+                    btnDisplay.Enabled = true;
+                    break;
+                default:
+                    txtMark.Text = "";
+                    lblMarkHeading.Text = "Individual Assesment";
+                    break;
+            }       
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            int highestMark = 0;
+            int averageMark = 0;
+
+            averageMark = CalcAverage(marks[0], marks[1], marks[2]);
+
+            highestMark = FindHighestMark(marks[0], marks[1], marks[2]);
+
+            Display(averageMark, highestMark);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDisplay.Enabled = false;
+        }
+    }
+}
